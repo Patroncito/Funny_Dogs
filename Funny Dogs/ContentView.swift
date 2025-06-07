d810a05dc920d556a654b114416a14a913f41d7f
Alfonso Patron <c9alfonsopatron@hotmail.com>
2025-06-07 00:38:54 -0600

    init commit - xcproject create

---
+++ Funny Dogs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Funny Dogs
+//
+//  Created by Alfonso Patron on 07/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Funny Dogs/Funny_DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Funny_DogsApp.swift
+//  Funny Dogs
+//
+//  Created by Alfonso Patron on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Funny_DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
